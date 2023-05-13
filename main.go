86cf80e24063fde0793dd269668c0ae3d1b48b48
David Landa <david.landa@protonmail.com>
2023-05-13 11:45:14 +0200

    Update output

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"regexp"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -11,11 +13,27 @@ func main() {
 
 	c := colly.NewCollector()
 
-	c.OnHTML("div.product-inline-item", func(e *colly.HTMLElement) {
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println(err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		log.Println("Visited", r.Request.URL)
+	})
+
+	c.OnHTML("h3.product-inline-title a[href]", func(e *colly.HTMLElement) {
+		c.Visit(e.Request.AbsoluteURL(e.Attr("href")))
+	})
+
+	c.OnHTML("div.item-detail-content", func(e *colly.HTMLElement) {
 		space := regexp.MustCompile(`\s+`)
-		s := space.ReplaceAllString(e.Text, " ")
+		texts := space.ReplaceAllString(strings.TrimSpace(e.Text), " ")
 		fmt.Println("---")
-		fmt.Println(s)
+		fmt.Println(strings.TrimSpace(texts))
+	})
+
+	c.OnHTML(".item-table tr", func(e *colly.HTMLElement) {
+		fmt.Println(strings.TrimSpace(e.Text))
 	})
 
 	c.Visit("https://vsezaodvoz.cz/inzeraty/dum-a-zahrada?region=14&type=1&new=1&with_photo=true&no_reservation=true&district=77")
